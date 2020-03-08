1979042dfa834c180868171656e60a40a22feec2
JanWielgus <jan.wielgus12@gmail.com>
2020-03-08 22:12:15 +0100

    Added new library flies (tested and working)
    New library flies after tests and comparison to the function pointers Tasker. Results are quite the same but this solution is a way more powerful.

---
+++ FC_ObjectTasker.h
@@ -0,0 +1,33 @@
+// FC_ObjectTasker.h
+/*
+    Created:	06/03/2020
+    Author:     Jan Wielgus
+*/
+
+#ifndef _FC_OBJECTTASKER_h
+#define _FC_OBJECTTASKER_h
+
+#include "arduino.h"
+#include <FC_Task.h>
+
+
+class FC_ObjectTasker
+{
+public:
+    FC_ObjectTasker(uint8_t maxTaskAmt);
+    virtual ~FC_ObjectTasker();
+    bool addTask(FC_Task* task);
+    virtual void run(); // this method is implemented but can be overridden
+
+
+protected:
+    const uint8_t MaxAmtOfTasks;
+    FC_Task** tasksArray; // static array of MaxAmtOfTasks size
+    uint8_t amtOfTasks = 0; // current amount of tasks (at most MaxAmtOfTasks)
+
+    uint32_t curTime = 0; // current time (used in run() method)
+};
+
+
+#endif
+
